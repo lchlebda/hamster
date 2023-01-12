c1cddb1b6d230a6857cacd201d9a3f4317ba4c41
Lukasz Chlebda <lukasz.chlebda@ig.com>
2023-01-12 23:16:31 +0100

    - add filtering on distance column

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -39,6 +39,7 @@ type IEditableCell = {
 
 const App: FC = (): ReactElement => {
 
+    const regexNumber = /\d+(\.\d+)?/
     const columnNames = ['Date', 'Sport', 'Title', 'Time', 'Rege time', 'HR', 'HR max', 'Cadence', 'Power', 'EF', 'TSS',
                          'Effort', 'Elevation', 'Speed', 'Distance', 'Notes'];
     const fields = ['date', 'type', 'title', 'time', 'regeTime', 'hr', 'hrMax', 'cadence', 'power', 'ef', 'tss',
@@ -119,9 +120,6 @@ const App: FC = (): ReactElement => {
                         setFilter((old = []) => [val ? parseInt(val, 10) : undefined, old[1]])
                     }}
                     placeholder={`${min}`}
-                    style={{
-                        width: '40px',
-                    }}
                 />
                 to
                 <input
@@ -132,9 +130,6 @@ const App: FC = (): ReactElement => {
                         setFilter((old = []) => [old[0], val ? parseInt(val, 10) : undefined])
                     }}
                     placeholder={`${max}`}
-                    style={{
-                        width: '40px',
-                    }}
                 />
             </div>
         )
@@ -158,7 +153,6 @@ const App: FC = (): ReactElement => {
                 onFilterClick(e, setFilter)
             }}>
                 <input
-                    value={filterValue.sort()[0]}
                     max={filterValue.sort()[filterValue.length-1]}
                     onBlur={e => {
                         let filterValues: string[] = []
@@ -171,9 +165,6 @@ const App: FC = (): ReactElement => {
                         setFilter(filterValues)
                     }}
                     placeholder={`${min}`}
-                    style={{
-                        width: '40px',
-                    }}
                 />
                 to
                 <input
@@ -189,9 +180,63 @@ const App: FC = (): ReactElement => {
                         setFilter(filterValues)
                     }}
                     placeholder={`${max}`}
-                    style={{
-                        width: '40px',
+                />
+            </div>
+        )
+    }
+
+    const DistanceRangeColumnFilter = ({
+                                        column: { filterValue = [], setFilter, preFilteredRows, id } ,
+                                    }: {column: UseFiltersColumnProps<String> & { id: string }}) => {
+        const [min, max] = useMemo(() => {
+            const firstNumber = preFilteredRows[0].values[id].match(regexNumber);
+            let min = preFilteredRows.length && firstNumber !== null ? firstNumber[0] : 0
+            let max = preFilteredRows.length && firstNumber !== null ? firstNumber[0] : 0
+            preFilteredRows.forEach(row => {
+                const match = row.values[id].match(regexNumber)
+                const num = match !== null ? Number(match[0]) : 0;
+                min = num < min ? num : min
+                max = num > max ? num : max
+            })
+            return [min, max]
+        }, [id, preFilteredRows])
+
+        return (
+            <div onClick={e => {
+                onFilterClick(e, setFilter)
+            }}>
+                <input
+                    max={filterValue.sort((a: number, b: number) => a - b)[filterValue.length-1]}
+                    onBlur={e => {
+                        let filterValues: number[] = []
+                        preFilteredRows.forEach(row => {
+                            const match = row.values[id].match(regexNumber)
+                            const num = match !== null ? Number(match[0]) : 0;
+                            if ((e.target.value === "" || num >= Number(e.target.value))
+                                && (e.target.max === "" || num <= Number(e.target.max))) {
+                                filterValues.push(num)
+                            }
+                        })
+                        setFilter(filterValues)
                     }}
+                    placeholder={`${min}`}
+                />
+                to
+                <input
+                    min={filterValue.sort((a: number, b: number) => a - b)[0]}
+                    onBlur={e => {
+                        let filterValues: number[] = []
+                        preFilteredRows.forEach(row => {
+                            const match = row.values[id].match(regexNumber)
+                            const num = match !== null ? Number(match[0]) : 0;
+                            if ((e.target.value === "" || num < Number(e.target.value))
+                                && (e.target.min === "" || num >= Number(e.target.min))) {
+                                filterValues.push(num)
+                            }
+                        })
+                        setFilter(filterValues)
+                    }}
+                    placeholder={`${max}`}
                 />
             </div>
         )
@@ -207,6 +252,9 @@ const App: FC = (): ReactElement => {
         if (header === 'Speed') {
             return SpeedRangeColumnFilter;
         }
+        if (header === 'Distance') {
+            return DistanceRangeColumnFilter;
+        }
         return undefined;
     }
 
@@ -217,7 +265,7 @@ const App: FC = (): ReactElement => {
         if (['Time', 'Rege time', 'HR', 'HR max', 'Cadence', 'Power', 'EF', 'TSS', 'Effort', 'Elevation'].includes(header)) {
             return 'between';
         }
-        if (header === 'Speed') {
+        if (header === 'Speed' || header === 'Distance') {
             return 'includesSome';
         }
         return '';
